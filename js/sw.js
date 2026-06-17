fb81494b20264efb9ad66f47b57daa68f585e07e
armntpwork <armntp.work@gmail.com>
2026-06-17 22:40:14 +0700

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -4,7 +4,7 @@ const STATIC = [
   '/places',
   '/emergency',
   '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600;700;800&display=swap',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap',
 ];
 
 self.addEventListener('install', e => {
